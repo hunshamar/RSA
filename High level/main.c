bd57f596f02799b00b9323cb95d814375b50eda8
Asgeir Hunshamar <asgeirhuns@gmail.com>
2019-09-30 16:22:04 +0200

    Added pseudocode

--- High level/main.c
+++ High level/main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
+
+// 1 Generate two large random primes, p and q, of approximate equal size such that their product n = pq 
+
+// 2. compute n = pq and ϕ = (p-1)(q-1)
+
+// 3. Choose an integer e, 1 < e < ϕ such that gdc(e,q) = 1 
+
+// 4. compute secret exponent d, 1 < d < ϕ such that ed = 1 mod ϕ
+
+// 5. The public key is (n,e) and the private key (d,p,q). Keep all the values d, p, q and ϕ secret. 
+//    [Sometimes the private key is written as (n,d) because you need the value of n when using d. 
+//     Other times we might write the key pair as ((N,e),d).]
+
+
+
+
 int main()
 {
 
-
+    
 
 
     printf("hei \n");
